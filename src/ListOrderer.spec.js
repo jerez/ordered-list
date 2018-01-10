40b616f2dc9093bb2c818c24a98b5712416cb8d3
Carlos Jerez <jerez@users.noreply.github.com>
2018-01-10 13:25:24 -0500

    Add react support

--- src/ListOrderer.spec.js
+++ src/ListOrderer.spec.js
@@ -28,25 +28,25 @@ describe('ListOrderer', () => {
 		const result = ListOrderer.order(expected, 'name', 'asc');
 
 		expect(result).toEqual(expected);
-  });
+	});
 
-  it('should return a list ordered by name desc', () => {
+	it('should return a list ordered by name desc', () => {
 		// expected == manually ordered mockList
 		const expected = [mockList[3], mockList[2], mockList[0], mockList[1]];
 		const result = ListOrderer.order(expected, 'name', 'desc');
 
-    expect(result).toEqual(expected);
-  });
+		expect(result).toEqual(expected);
+	});
 
-  it('should return a list ordered by date asc', () => {
+	it('should return a list ordered by date asc', () => {
 		// expected == manually ordered mockList
 		const expected = [mockList[2], mockList[1], mockList[3], mockList[0]];
 		const result = ListOrderer.order(expected, 'date', 'asc');
 
-    expect(result).toEqual(expected);
-  });
+		expect(result).toEqual(expected);
+	});
 
-  it('should return a list ordered by date desc', () => {
+	it('should return a list ordered by date desc', () => {
 		// expected == manually ordered mockList
 		const expected = [mockList[0], mockList[3], mockList[1], mockList[2]];
 		const result = ListOrderer.order(expected, 'date', 'desc');
