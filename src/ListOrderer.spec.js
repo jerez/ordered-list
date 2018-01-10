b7aa7569b31b418983bceb26c7a40dedf8548351
Carlos Jerez <jerez@users.noreply.github.com>
2018-01-10 13:25:24 -0500

    Adding storybook

--- src/ListOrderer.spec.js
+++ src/ListOrderer.spec.js
@@ -1,25 +1,7 @@
 /* @flow */
 
 import ListOrderer from './ListOrderer';
-
-const mockList = [
-	{
-		name: 'b',
-		date: new Date('2018-01-11T04:00:00'),
-	},
-	{
-		name: 'a',
-		date: new Date('2018-01-11T02:00:00'),
-	},
-	{
-		name: 'c',
-		date: new Date('2018-01-11T01:00:00'),
-	},
-	{
-		name: 'd',
-		date: new Date('2018-01-11T03:00:00'),
-	},
-];
+import mockList from './__mocks__/mock-list';
 
 describe('ListOrderer', () => {
 	it('should return a list ordered by name asc', () => {
--- src/OrderedList.spec.js
+++ src/OrderedList.spec.js
@@ -8,34 +8,16 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import OrderedList from './OrderedList';
+import mockList from './__mocks__/mock-list';
+
 
 configure({ adapter: new Adapter() });
-
-const mockList = [
-	{
-		name: 'b',
-		date: new Date('2018-01-11T04:00:00'),
-	},
-	{
-		name: 'a',
-		date: new Date('2018-01-11T02:00:00'),
-	},
-	{
-		name: 'c',
-		date: new Date('2018-01-11T01:00:00'),
-	},
-	{
-		name: 'd',
-		date: new Date('2018-01-11T03:00:00'),
-	},
-];
 
 const mockProps = {
 	list: mockList,
 	sortBy: 'name',
 	sortOrder: 'asc',
 };
-
 
 describe('OrderedList', ()=> {
 	it('renders without crashing', () => {
