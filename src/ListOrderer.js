d32c5f9231b39ffd78c5e1c5d93f31cb636d487d
Carlos Jerez <jerez@users.noreply.github.com>
2018-01-10 13:25:24 -0500

    ListOrderer implementation

--- src/ListOrderer.js
+++ src/ListOrderer.js
@@ -1,13 +1,15 @@
 /* @flow */
 
-type DateNameArrayType = Array<{date: Date, name: string}>;
+import {orderBy} from 'lodash';
+
+export type sortByType = 'name' | 'date';
+export type sortOrderType = 'asc' | 'desc'
+export type DateNameArrayType = Array<{date: Date, name: string}>;
 
 class ListOrderer {
-	order(list: DateNameArrayType): DateNameArrayType{
-		return list;
+	order(list: DateNameArrayType, sortBy:sortByType, sortOrder:sortOrderType): DateNameArrayType{
+    return orderBy(list, sortBy, sortOrder);
 	}
 }
 
 export default new ListOrderer();
-
-
--- src/ListOrderer.spec.js
+++ src/ListOrderer.spec.js
@@ -22,15 +22,35 @@ const mockList = [
 ];
 
 describe('ListOrderer', () => {
-	it('should return a list ordered by name', () => {
+	it('should return a list ordered by name asc', () => {
 		// expected == manually ordered mockList
 		const expected = [mockList[1], mockList[0], mockList[2], mockList[3]];
-		const result = ListOrderer.order(expected);
+		const result = ListOrderer.order(expected, 'name', 'asc');
 
-		expect(result).toBe(expected);
-		expect(result[0]).toBe(expected[1]);
-		expect(result[1]).toBe(expected[1]);
-		expect(result[2]).toBe(expected[2]);
-		expect(result[3]).toBe(expected[3]);
+		expect(result).toEqual(expected);
+  });
+
+  it('should return a list ordered by name desc', () => {
+		// expected == manually ordered mockList
+		const expected = [mockList[3], mockList[2], mockList[0], mockList[1]];
+		const result = ListOrderer.order(expected, 'name', 'desc');
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a list ordered by date asc', () => {
+		// expected == manually ordered mockList
+		const expected = [mockList[2], mockList[1], mockList[3], mockList[0]];
+		const result = ListOrderer.order(expected, 'date', 'asc');
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a list ordered by date desc', () => {
+		// expected == manually ordered mockList
+		const expected = [mockList[0], mockList[3], mockList[1], mockList[2]];
+		const result = ListOrderer.order(expected, 'date', 'desc');
+
+		expect(result).toEqual(expected);
 	});
 });
