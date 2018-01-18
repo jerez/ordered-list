0e6bba3c5fc8d19ec9cc9687aab973bff2eddc13
Carlos Jerez <jerez@users.noreply.github.com>
2018-01-18 07:47:34 -0500

    Remove lodash

--- src/ListOrderer.js
+++ src/ListOrderer.js
@@ -1,11 +1,32 @@
 /* @flow */
 
-import {orderBy} from 'lodash';
-import {type DateNameArrayType, type SortByType, type SortOrderType} from './Types';
+import {
+	type DateNameArrayType,
+	type SortByType,
+	type SortOrderType,
+	type DateNameItem,
+} from './Types';
 
 class ListOrderer {
 	order(list: DateNameArrayType, sortBy: SortByType, sortOrder: SortOrderType): DateNameArrayType{
-		return orderBy(list, sortBy, sortOrder);
+		const reverse: boolean = sortOrder === 'desc';
+		if(sortBy === 'name'){
+			return list.sort(this._sortAlpha.bind(this, reverse));
+		} else {
+			return list.sort(this._sortDate.bind(this, reverse));
+		}
+	}
+
+	_sortDate(reverse: boolean, a: DateNameItem, b: DateNameItem){
+		return reverse ? b.date - a.date : a.date - b.date;
+	}
+
+	_sortAlpha(reverse: boolean, a: DateNameItem, b: DateNameItem){
+		const stringA = a.name.toLowerCase();
+		const stringB = b.name.toLowerCase();
+		if (stringA < stringB) return reverse ? 1 : -1;
+		if (stringB > stringB) return reverse ? -1 : 1;
+		return 0;
 	}
 }
 
